0cfd425ace901b498e6bf6822ef3333da56f0ec8
ghazalhp <ghazalhasanpour@gmail.com>
2017-02-10 13:36:29 +0330

    Add files via upload
    not correcttly work

--- entebagh.java
+++ entebagh.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class entebagh {
+	static int j = 0;
+	static int w = 0;
 
 	static boolean bfs(int[][] g, int n) {
 		Queue<Integer> q = new LinkedList<Integer>();
@@ -24,6 +27,10 @@ public class entebagh {
 						break;
 					}
 					color[i] = -color[node];
+					if (color[i] == 1)
+						j++;
+					else
+						w++;
 				}
 				if (!check)
 					break;
@@ -31,6 +38,41 @@ public class entebagh {
 		}
 		return check;
 
+	}
+
+	static boolean bpm(int[][] g, int u, boolean seen[], int matchR[]) {
+
+		for (int v = 0; v < j; v++) {
+
+			if (g[u][v] > 0 && !seen[v]) {
+				seen[v] = true;
+
+				if (matchR[v] < 0 || bpm(g, matchR[v], seen, matchR)) {
+					matchR[v] = u;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	static int maxBPM(int[][] g, int n) {
+
+		int matchR[] = new int[j];
+
+		for (int i = 0; i < j; ++i)
+			matchR[i] = -1;
+
+		int result = 0;
+		for (int u = 0; u < w; u++) {
+			boolean seen[] = new boolean[j];
+			for (int i = 0; i < j; ++i)
+				seen[i] = false;
+
+			if (bpm(g, u, seen, matchR))
+				result++;
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
@@ -46,9 +88,8 @@ public class entebagh {
 		if (!bfs(g, n)) {
 			System.out.println("dobakhsi nist");
 		} else {
-			System.out.println("odafez");
+			System.out.println(maxBPM(g, n));
 		}
 
 	}
-
 }
